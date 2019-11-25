1c48f6ae4ca95064133650a5720d2b36da1e365b
Ashley <brackett@pdx.edu>
2019-11-24 19:04:30 -0800

    about update

--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,12 @@ $('.slideshow').square1({
 	caption: 'none',
 });
 
-
+// failed hamburger
+$(document).ready(function(){
+	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
+		$(this).toggleClass('open');
+	});
+});
 
 
 
