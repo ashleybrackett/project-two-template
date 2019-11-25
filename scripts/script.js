fd67995461dc305947ddc6b64404851b7cc1eac2
Ashley <brackett@pdx.edu>
2019-11-24 19:43:46 -0800

    more stuff

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,52 @@
 $(function() {
 // Your Code from here on down. Don't delete that line above!
   
+$(".slider").slick({
+
+  // normal options...
+  infinite: false,
+
+  // the magic
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
+});
+
+// On swipe event
+$('.your-element').on('swipe', function(event, slick, direction){
+  console.log(direction);
+  // left
+});
+
+// On edge hit
+$('.your-element').on('edge', function(event, slick, direction){
+  console.log('edge was hit')
+});
+
+// On before slide change
+$('.your-element').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+  console.log(nextSlide);
+});
+
 $('.slideshow').square1({
 	animation: 'slide',
 	transition_time: 2000,
@@ -23,6 +69,7 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 	});
 });
+
 
 
 
