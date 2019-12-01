c3168f09a7c82b2045637c4309bfe8f9716e8e4f
Ashley <brackett@pdx.edu>
2019-12-01 14:15:59 -0800

    chachachanges

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 // your script file for adding your own jquery
+
+
 $(function() {
 // Your Code from here on down. Don't delete that line above!  
 $(".slider").slick({
@@ -47,28 +49,9 @@ $('.your-element').on('beforeChange', function(event, slick, currentSlide, nextS
   console.log(nextSlide);
 });
 
-$('.slideshow').square1({
-	animation: 'slide',
-	transition_time: 2000,
-	prev_next_nav: 'hover',
-	dots_nav: 'hover',
-	fill_mode: 'contain',
-	auto_start: true,
-	start_delay: 25,
-	pause_on_hover: true,
-	theme: 'light',
-	prev_next_nav: 'inside',
-	dots_nav: 'inside',
-	caption: 'none',
-});
 
-// failed hamburger
-$(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	});
-});
 
+//hamburger nav//
 
 
 
